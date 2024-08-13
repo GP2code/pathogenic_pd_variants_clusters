98042c549e95426de9fc7a33b5eb2080ef5b1bc2
mkoretsky1 <mathew.koretsky1@gmail.com>
2024-08-13 15:38:31 -0400

    dropping more cols

--- cluster_plots.py
+++ cluster_plots.py
@@ -122,7 +122,7 @@ metric1,metric2 = st.columns([1,1])
 num_snps = len(merge1['snpID'].unique())
 num_sample_metrics = len(merge1['Sample_ID'].unique())
 
-merge1 = merge1.drop(columns=['GenTrain_Score','chromosome','position'])
+merge1 = merge1.drop(columns=['GenTrain_Score','chromosome','position','merge_id'])
 
 # get SNP options
 snp_options = ['Select SNP!']+[snp for snp in merge1['SNP'].unique()]
